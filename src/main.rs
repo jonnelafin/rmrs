67a9a9cf74d57131b6517314bb28ecc0ed378a55
Elias Eskelinen <elias.eskelinen@pm.me>
2020-10-02 16:50:54 +0300

    nice

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use minifb::{Key, Scale, ScaleMode, Window, WindowOptions};
 
-const WIDTH: usize = 100;
-const HEIGHT: usize = 100;
+const WIDTH: usize = 900;
+const HEIGHT: usize = 900;
 
 fn main() {
     let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
@@ -24,10 +24,12 @@ fn main() {
 
 
     window.set_background_color(0, 0, 20);
-	let mut time = 0;
+	let mut time = 10;
     while window.is_open() && !window.is_key_down(Key::Escape) {
         for i in 0..buffer.len() {
-            buffer[i] = i as u32 + time ^ 10;
+        	let y = i / WIDTH;
+        	let x = i % HEIGHT;
+            buffer[i] = ( (x as u32 + y as u32) as f32  % (time as f32 /2.0) ) as u32;//i as u32 + time ^ 10;
         }
 
         // We unwrap here as we want this code to exit if it fails
